a83c02d7994a9e090f026d6bfd53f7deb8d27c6a
Jasmine Coder <jasminethecoder@gmail.com>
2020-04-19 09:39:44 -0500

    new stuff

--- contact/gmail-contact.php
+++ contact/gmail-contact.php
@@ -1,136 +1,65 @@
 <?php
 
-/*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require './PHPMailer-master/vendor/autoload.php';
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
-
-// an email address that will be in the From field of the email.
-$fromEmail = $_POST['email'];
-$fromName = $_POST['name'];
-
-// an email address that will receive the email with the output of the form
-$sendToEmail = 'developmentequinox@gmail.com';
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name:', 'email' => 'Email:', 'message' => 'Message:');
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Successfully submitted - we will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
-*  LET'S DO THE SENDING
-*/
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
-try
-{
-    
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
+function configureMailerSMTPSettings($mailer) {
+    $mailer->IsSMTP();
+    $mailer->SMTPAuth='true';
+    $mailer->SMTPSecure =  PHPMailer::ENCRYPTION_STARTTLS;
+    $mailer->Host = 'smtp.gmail.com';
+    $mailer->Port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
+    $mailer->Username = "developmentequinox@gmail.com";
+    $mailer->Password = "!adorabledog1";
+}
+
+function getMessageBody() {
     $emailTextHtml .= "<h3>New message from the w3newbie Theme:</h3><hr>";
     $emailTextHtml .= "<table>";
-
+    $expectedFields = array('name' => 'Name:', 'email' => 'Email:', 'message' => 'Message:');
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email
-        if (isset($fields[$key])) {
-            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+        if (isset($expectedFields[$key])) {
+            $emailTextHtml .= "<tr><th>$expectedFields[$key]</th><td>$value</td></tr>";
         }
     }
     $emailTextHtml .= "</table><hr>";
     $emailTextHtml .= "<p>Have a great day!<br><br>Sincerely,<br><br>w3newbie Theme</p>";
-    
-    $mail = new PHPMailer(true);
+    return $emailTextHtml;
+}
 
-//************** IMPORTANT
+function setEmailContent($mailer) {
+    if (count($_POST) == 0) throw new \Exception('Form is empty');
+    $mailer->AddAddress("developmentequinox@gmail.com");
+    $mailer->From = $_POST['email'];
+    $mailer->FromName = $_POST['name'];
+    $mailer->AddReplyTo($_POST['email'], $_POST['name']);
+    $mailer->Subject = 'New message from contact form';
+    $mailer->Body = getMessageBody();
+    $mailer->isHTML(true);
+    //$mailer->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+}
 
-    // write you email configuration below and see the important steps for it to work.
-
-    // IMPORTANT: You must turn off 2 step verification on your google/gmail account:
-    // Instructions: https://support.google.com/accounts/answer/1064203?co=GENIE.Platform%3DDesktop&hl=en
-    // Security Page: https://myaccount.google.com/security
-
-    // IMPORTANT You must also "Allow less secure apps" on your google/gmail account:
-    // https://myaccount.google.com/lesssecureapps
-
-//************** IMPORTANT
-
-$mail->IsSMTP();
-                $mail->SMTPAuth='true';
-                $mail->SMTPSecure =  PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Host = 'smtp.gmail.com';
-                $mail->Port = "587"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
-                
-                $mail->Username = "developmentequinox@gmail.com";
-                $mail->Password = "!adorabledog1";
-
-                $mail->From = $fromEmail;
-                $mail->FromName = $fromName;
-                $mail->AddAddress($sendToEmail);
-                $mail->AddReplyTo($fromEmail,$fromName);
-
-
-
-
-
-
-    // end email configuration
-
-
-
-
-
-
-
-
-
-
-
-
-    $mail->Subject = 'New message from contact form';
-
-    $mail->Body = $emailTextHtml;
-    $mail->isHTML(true);
-    //$mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-    
-    
-    if(!$mail->send()) {
-        throw new \Exception('Email send failed. ' . $mail->ErrorInfo);
+function sendMessage() {
+    try {
+        if (!$mailer->send()) {
+            throw new \Exception('Email send failed. ' . $mailer->ErrorInfo);
+        }
+    } catch (\Exception $e) {
+        return $e->getMessage();
     }
-    
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+    return null;    
 }
 
+$mailer = new PHPMailer(true);
+configureMailerSMTPSettings($mailer);
+setEmailContent($mailer);
+$errorMessage = sendMessage($mailer);
+echo($errorMessage ? $errorMessage : 'Message was successfully sent.');
 
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-    
-    header('Content-Type: application/json');
-    
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
-}
 ?>
