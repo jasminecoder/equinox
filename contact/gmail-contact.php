f45e5ef21d3ad00a7018aadd6b744684ebab1217
Jasmine Coder <jasminethecoder@gmail.com>
2020-04-19 09:49:09 -0500

    add debugging

--- contact/gmail-contact.php
+++ contact/gmail-contact.php
@@ -4,8 +4,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer-master/vendor/autoload.php';
-// require 'vendor/autoload.php';
+// require './PHPMailer-master/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 error_reporting(E_ALL & ~E_NOTICE);
 
